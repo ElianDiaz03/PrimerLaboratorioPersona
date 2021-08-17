7f5544f7c89a1935bce2efdb5f72b58be5866784
ElianDiaz03 <87671672+ElianDiaz03@users.noreply.github.com>
2021-08-17 10:55:37 -0500

    cuarta Funcion EA

---
+++ Persona/Persona/ClsPersona.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Entidades
+{
+    class ClsPersona
+    {
+
+        // Propiadades de una persona 
+
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string sexo;
+        private string nacionalidad;
+
+        // Acciones de una persona 
+
+        private string habla;
+        private string respira;
+        private string camina;
+        private string observa;
+        private string pensa;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public string Sexo { get => sexo; set => sexo = value; }
+        public string Nacionalidad { get => nacionalidad; set => nacionalidad = value; }
+        public string Habla { get => habla; set => habla = value; }
+        public string Respira { get => respira; set => respira = value; }
+        public string Camina { get => camina; set => camina = value; }
+        public string Observa { get => observa; set => observa = value; }
+        public string Pensa { get => pensa; set => pensa = value; }
+    }
+}
---
+++ Persona/Persona/NClsPersona.cs
@@ -0,0 +1,44 @@
+﻿using Persona.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona.Negocio
+{
+    class NClsPersona
+    {
+        public String Hablar(ClsPersona persona)
+        {
+            return " El nombre de una persona " + persona.Nombre + "se define por su sexo " + persona.Sexo + " Segun hablan estudios ";
+
+
+        }
+        public String Nacionalidad(ClsPersona persona)
+        {
+            return " El apellido Garcia " + persona.Apellido + " Es de nacionalidad" + persona.Nacionalidad + " Española ";
+
+
+
+        }
+        public String Edad(ClsPersona persona)
+        {
+            return " Minerva tiene la edad de 18 años " + persona.Edad + " y es de nacionalidad Argentina " + persona.Nacionalidad;
+
+
+
+        }
+
+        public String Edad(ClsPersona persona)
+        {
+            return " El niño solo tiene 8 años " + persona.edad + " y ya puede hablar 2 idiomas " + persona.hablar;
+
+
+
+
+
+
+        }
+    }
+}
