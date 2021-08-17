b877b0dbb8f1f7775ab3c3706d553cd10aec0f67
Wenpineda <cp0428032021@unab.edu.sv>
2021-08-17 10:02:51 -0600

    Funciones WCH

--- Persona/Persona/Negocio/NClsPersona.cs
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,22 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
-    }
+        public String Hablar (ClsPersona persona)
+        {
+        return " El nombre de una persona " + persona.Nombre + "se define por su sexo " + persona.Sexo+ " Segun hablan estudios ";
+         
+
+        }
+         public String Nacionalidad (ClsPersona persona)
+        {
+          return " El apellido Garcia " + persona.Apellido+ " Es de nacionalidad" + persona.Nacionalidad + " Española ";
+
+
+    
+         }
+         public String Edad(ClsPersona persona)
+         {
+          return " Minerva tiene la edad de 18 años " + persona.Edad + " y es de nacionalidad Argentina " + persona.Nacionalidad;
+
+    }    }
 }
